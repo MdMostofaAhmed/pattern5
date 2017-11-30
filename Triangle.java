948ade4917cf57ab89791f174aed117461905a44
Mostofa Ahmed <34137210+MdMostofaAhmed@users.noreply.github.com>
2017-11-30 23:58:01 +0600

    Add files via upload

---
+++ Triangle.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Triangle {
+	public static void main(String args[])
+	{
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Enter an Integer: ");
+		int n=scan.nextInt();
+		
+		for (int i=1; i<=n; i++)
+		{
+			for (int j=i; j<n; j++)
+			{
+				System.out.print(" ");
+			}
+			for (int k=1; k<(i*2); k++)
+			{
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+	}
+}
